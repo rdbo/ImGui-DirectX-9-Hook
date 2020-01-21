d906e13c9a11d5b02cf285fbc404988902b3e00b
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-21 13:35:46 -0300

    Add files via upload

---
+++ ImGui DirectX 9 Hook/main.cpp
@@ -0,0 +1,64 @@
+#include "includes.h"
+#include "phook.h"
+
+HWND window;
+void* pDevice[119];
+PTR EndSceneAddress;
+EndScene oEndScene;
+WNDPROC oWndProc;
+
+void InitImGui(LPDIRECT3DDEVICE9 device)
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(device);
+}
+
+bool initialized = false;
+long __stdcall PHook::hkEndScene(LPDIRECT3DDEVICE9 device)
+{
+	if (!initialized)
+	{
+		InitImGui(device);
+		initialized = true;
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("ImGui Window");
+	ImGui::End();
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+	return oEndScene(device);
+}
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT __stdcall PHook::WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)PHook::Init, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)PHook::Shutdown, hMod, 0, nullptr);
+		break;
+	}
+	return TRUE;
+}
